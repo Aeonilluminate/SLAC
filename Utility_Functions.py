28bc94fa9041763023817641d57d9639571d1ca9
Nathan Acosta <65561902+Aeonilluminate@users.noreply.github.com>
2023-08-08 14:58:16 -0700

    Added additional transformation method New method implements noise in the form of random lines and speckles

---
+++ Utility_Functions.py
@@ -0,0 +1,23 @@
+
+def bresenham_line(x0, y0, x1, y1):
+    """Generate points on a line from (x0, y0) to (x1, y1) using Bresenham's algorithm."""
+    points = []
+    dx = abs(x1 - x0)
+    dy = abs(y1 - y0)
+    sx = 1 if x0 < x1 else -1
+    sy = 1 if y0 < y1 else -1
+    err = dx - dy
+
+    while True:
+        points.append((y0, x0))
+        if x0 == x1 and y0 == y1:
+            break
+        e2 = 2 * err
+        if e2 > -dy:
+            err -= dy
+            x0 += sx
+        if e2 < dx:
+            err += dx
+            y0 += sy
+
+    return points
